9c320f8b1b892e7187497bd0dec4b020704dcfec
42nd <90954419+42nd@users.noreply.github.com>
2021-10-18 12:10:48 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <link href="http://dcoding.kr/style/index.html.css" rel="stylesheet" />
+    <link href="index.php.css" rel="stylesheet" />
 </head>
 <body>
     <?php
